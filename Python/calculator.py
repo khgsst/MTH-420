c1f0928bc86cc12ce07be564285d4e9f26ec4cbd
[Labib Zakaria] <[zakarial@oregonstate.edu]>
2021-01-22 11:56:10 -0800

    lab2

---
+++ Python/calculator.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def sum(a,b):
+    s=a+b
+    return s
+"""Yields sum of two given integers"""
+
+def prod(u,v):
+    p=u*v
+    return p
+"""Yields product of two given integers"""
