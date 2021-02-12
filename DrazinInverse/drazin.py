6314036b4cf853682d60004d8356f9fb149483f2
[Labib Zakaria] <[zakarial@oregonstate.edu]>
2021-02-12 11:14:24 -0800

    Update DrazinInerse

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -1,12 +1,14 @@
 # drazin.py
 """Volume 1: The Drazin Inverse.
-<Name>
-<Class>
-<Date>
+<Labib Zakaria>
+<MTH 420>
+<2/12/21>
 """
 
 import numpy as np
 from scipy import linalg as la
+from scipy.sparse.csgraph import laplacian as lap
+from numpy import linalg as nla
 
 
 # Helper function for problems 1 and 2.
@@ -50,9 +52,19 @@ def is_drazin(A, Ad, k):
     Returns:
         (bool) True of Ad is the Drazin inverse of A, False otherwise.
     """
+    b=np.allclose(A@Ad,Ad@A) and np.allclose(nla.matrix_power(A,k+1)@Ad,nla.matrix_power(A,k)) and np.allclose(Ad@A@Ad,Ad)
+    print(b)
+    return b
     raise NotImplementedError("Problem 1 Incomplete")
 
-
+A=np.array([[1,3,0,0],[0,1,3,0],[0,0,1,3],[0,0,0,0]])
+Ad=np.array([[1,-3,9,81],[0,1,-3,-18],[0,0,1,3],[0,0,0,0]])
+k=1
+is_drazin(A,Ad,k)
+A=np.array([[1,1,3],[5,2,6],[-2,-1,-3]])
+Ad=np.array([[0,0,0],[0,0,0],[0,0,0]])
+k=3
+is_drazin(A,Ad,k)
 # Problem 2
 def drazin_inverse(A, tol=1e-4):
     """Compute the Drazin inverse of A.
@@ -63,9 +75,31 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
+    (n,n)=A.shape    
+    f=lambda x:abs(x)>tol
+    T1,Q1,k1=la.schur(A,sort=f)
+    g=lambda x:abs(x)<=tol
+    T2,Q2,k2=la.schur(A,sort=g)
+    U=np.column_stack((Q1[:,:k1],Q2[:,:(n-k1)]))
+    UI=la.inv(U)
+    V=U@A@UI
+    Z=np.zeros((n,n))
+    if k1!=0:
+        MI=la.inv(V[:k1,:k1])
+        Z[:k1,:k1]=MI
+    P=U@Z@UI
+    print(P)
+    return P 
     raise NotImplementedError("Problem 2 Incomplete")
 
-
+A=np.array([[1,3,0,0],[0,1,3,0],[0,0,1,3],[0,0,0,0]])
+Ad=drazin_inverse(A, tol=1e-4)
+k=1
+is_drazin(A,Ad,k)
+A=np.array([[1,1,3],[5,2,6],[-2,-1,-3]])
+Ad=drazin_inverse(A, tol=1e-4)
+k=3
+is_drazin(A,Ad,k)
 # Problem 3
 def effective_resistance(A):
     """Compute the effective resistance for each node in a graph.
@@ -77,14 +111,28 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
+    (n,n)=A.shape
+    R=np.zeros((n,n))
+    I=np.zeros((n,n))+np.diag(np.diag(np.ones((n,n))))
+    print("R="+str(R))
+    print("I="+str(I))
+    for i in range(n):
+        L=lap(A)
+        print(L)
+        L[i,:]=I[i,:]
+        LD=drazin_inverse(L, tol=1e-4)
+        R[i,:]=LD[i,:]
+        R[i,i]=0
+    print(R)
+    return R
     raise NotImplementedError("Problem 3 Incomplete")
-
-
+A=np.array([[0,1],[1,0]])
+effective_resistance(A)
 # Problems 4 and 5
 class LinkPredictor:
     """Predict links between nodes of a network."""
 
-    def __init__(self, filename='social_network.csv'):
+def __init__(self, filename='social_network.csv'):
         """Create the effective resistance matrix by constructing
         an adjacency matrix.
 
@@ -94,7 +142,7 @@ class LinkPredictor:
         raise NotImplementedError("Problem 4 Incomplete")
 
 
-    def predict_link(self, node=None):
+def predict_link(self, node=None):
         """Predict the next link, either for the whole graph or for a
         particular node.
 
@@ -110,10 +158,8 @@ class LinkPredictor:
         Raises:
             ValueError: If node is not in the graph.
         """
-        raise NotImplementedError("Problem 5 Incomplete"
-
-
-    def add_link(self, node1, node2):
+        raise NotImplementedError("Problem 5 Incomplete")
+def add_link(self, node1, node2):
         """Add a link to the graph between node 1 and node 2 by updating the
         adjacency matrix and the effective resistance matrix.
 
